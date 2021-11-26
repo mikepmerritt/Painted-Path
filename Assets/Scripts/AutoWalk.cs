3394e0424ed53571b9a283db11c4693420c6b92a
mikepmerritt <mikepmerritt@gmail.com>
2021-11-26 17:37:19 -0500

    added artist and enemy prefabs

---
+++ Assets/Scripts/AutoWalk.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoWalk : MonoBehaviour
+{
+    public bool StopOnEdges;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
