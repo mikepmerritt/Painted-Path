a2c35f0363880c8f3104102b997b18f510605775
PokeSpencerMx <91617455+PokeSpencerMx@users.noreply.github.com>
2021-11-27 13:39:05 -0500

    Player Moves In Unfinished Scene

--- Assets/Scripts/AutoWalk.cs
+++ Assets/Scripts/AutoWalk.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class AutoWalk : MonoBehaviour
 {
     public bool StopOnEdges;
+    float WalkSpeed;
+    private Rigidbody2D rb;
+    
 
     void Start()
     {
+        WalkSpeed = 2;
+
         
+
     }
 
     void Update()
     {
-        
+        GetComponent<Rigidbody2D>().velocity = new Vector2(WalkSpeed, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        WalkSpeed = 2;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        WalkSpeed = 0;
     }
 }
