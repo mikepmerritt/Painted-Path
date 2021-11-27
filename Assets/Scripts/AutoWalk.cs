b73c6b4962ac0d570696e7426795316e520b799a
PokeSpencerMx <91617455+PokeSpencerMx@users.noreply.github.com>
2021-11-27 18:44:12 -0500

    Artist now moves back a little after bumping but then stops

--- Assets/Scripts/AutoWalk.cs
+++ Assets/Scripts/AutoWalk.cs
@@ -7,12 +7,13 @@ public class AutoWalk : MonoBehaviour
     public bool StopOnEdges;
     float WalkSpeed;
     private Rigidbody2D rb;
+    bool OnGround;
     
 
     void Start()
     {
         WalkSpeed = 2;
-
+        OnGround = false;
         
 
     }
@@ -20,15 +21,27 @@ public class AutoWalk : MonoBehaviour
     void Update()
     {
         GetComponent<Rigidbody2D>().velocity = new Vector2(WalkSpeed, 0);
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        WalkSpeed = 2;
+        if(OnGround == false)
+        {
+            WalkSpeed = 2;
+            OnGround = true;
+        }
+        else
+        {
+            WalkSpeed = -2;
+            OnGround = true;
+        }
+        //WalkSpeed = 2;
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
+        OnGround = false;
         WalkSpeed = 0;
     }
 }
