876fb1360ff6aff7db949ffc4b0337ecb2776398
PokeSpencerMx <91617455+PokeSpencerMx@users.noreply.github.com>
2021-11-27 21:29:15 -0500

    Temporary Way to Bounce Off Walls

--- Assets/Scripts/AutoWalk.cs
+++ Assets/Scripts/AutoWalk.cs
@@ -14,7 +14,8 @@ public class AutoWalk : MonoBehaviour
 
     void Start()
     {
-        SpeedNumber = 2;
+        WalkSpeed = 2;
+        SpeedNumber = WalkSpeed;
         OnGround = false;
         direction = 1;
 
@@ -29,11 +30,24 @@ public class AutoWalk : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         SpeedNumber = 2;
+        if (OnGround == false)
+        {
+            OnGround = true;
+        }
+        else
+        {
+            direction = -1 * direction;
+        }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
         OnGround = false;
         SpeedNumber = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        direction = -1 * direction;
     }
 }
